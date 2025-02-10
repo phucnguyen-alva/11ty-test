c71186096791c36b652a335924e4e56166a0fabe
Jeff Siarto <jsiarto@gmail.com>
2025-02-10 15:53:17 +0000

    Shortcode and data examples

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 };
